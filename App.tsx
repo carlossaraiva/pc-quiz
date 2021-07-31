e163878de19b2da5affae588e4c7771fe4d371c5
Carlos Saraiva <carlosedosaraiva@gmail.com>
2021-07-31 18:19:05 -0300

    integrate app with remote service

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, ScrollView, FlatList } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StackScreenProps } from "@react-navigation/stack";
@@ -12,14 +12,13 @@ import {
   ActivityIndicator,
   Card,
   Paragraph,
-  DataTable,
 } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 import create from "zustand";
 import shallow from "zustand/shallow";
 import { configurePersist } from "zustand-persist";
-import * as WebBrowser from "expo-web-browser";
+import "react-native-url-polyfill/auto";
 
 type SurveyState = {
   device: string;
@@ -32,6 +31,8 @@ const { persist, purge } = configurePersist({
   storage: AsyncStorage,
   rootKey: "root",
 });
+
+const queryClient = new QueryClient();
 
 const surveyStore = create<SurveyState>(
   persist(
@@ -67,38 +68,38 @@ const USAGE_ANSWERS: StringMap = {
 const RESPONSES = {
   pc: {
     "video-editor": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk015nf1QkgIp2_1TaZM-kcplr00bHQ:1615675330800&q=PC&tbas=0&tbs=vw:g,mr:1,cat:325,pdtr0:1020653%7C3.9000000953674316%24,price:1,ppr_min:6000&sa=X&ved=0ahUKEwiSlKjRq67vAhUOIrkGHRIoBHkQvSsIvQcoAw&biw=1920&bih=937",
+      query: "PC+intel+i9+32GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk015nf1QkgIp2_1TaZM-kcplr00bHQ:1615675330800&q=PC&tbas=0&tbs=vw:g,mr:1,cat:325,pdtr0:1020653%7C3.9000000953674316%24,price:1,ppr_min:6000&sa=X&ved=0ahUKEwiSlKjRq67vAhUOIrkGHRIoBHkQvSsIvQcoAw&biw=1920&bih=937",
     },
     "software-development": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk03huVIYMtgjCYFlVTt0v9YYfw2eaQ:1615675272485&q=PC&tbs=vw:g,mr:1,pdtr0:1020655%7C64.0%24160.0,pdtr1:728839%7C728842,pdtr2:1020654%7C16.0%2416.0,cat:325,pdtr3:950630%7C950631&sa=X&ved=0ahUKEwjF9cC1q67vAhXOJrkGHXF-D8oQsysIoQMoAA&biw=1920&bih=937",
+      query: "PC+intel+i7+32GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk03huVIYMtgjCYFlVTt0v9YYfw2eaQ:1615675272485&q=PC&tbs=vw:g,mr:1,pdtr0:1020655%7C64.0%24160.0,pdtr1:728839%7C728842,pdtr2:1020654%7C16.0%2416.0,cat:325,pdtr3:950630%7C950631&sa=X&ved=0ahUKEwjF9cC1q67vAhXOJrkGHXF-D8oQsysIoQMoAA&biw=1920&bih=937",
     },
     "office-tools": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk02ZBwi_tLoz6-vp9W57ZDC1TsZ3FA:1615675389597&q=PC&tbas=0&tbs=vw:g,mr:1,pdtr0:1010563%7C1010566,pdtr1:728839%7C728841,cat:325,pdtr2:728797%7C764654&sa=X&ved=0ahUKEwj27aztq67vAhXTLLkGHZ4kB4oQsysIwgcoAA&biw=1920&bih=937",
+      query: "PC+intel+i3+8GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk02ZBwi_tLoz6-vp9W57ZDC1TsZ3FA:1615675389597&q=PC&tbas=0&tbs=vw:g,mr:1,pdtr0:1010563%7C1010566,pdtr1:728839%7C728841,cat:325,pdtr2:728797%7C764654&sa=X&ved=0ahUKEwj27aztq67vAhXTLLkGHZ4kB4oQsysIwgcoAA&biw=1920&bih=937",
     },
     internet: {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk02ZBwi_tLoz6-vp9W57ZDC1TsZ3FA:1615675389597&q=PC&tbas=0&tbs=vw:g,mr:1,pdtr0:1010563%7C1010566,pdtr1:728839%7C728841,cat:325,pdtr2:728797%7C764654&sa=X&ved=0ahUKEwj27aztq67vAhXTLLkGHZ4kB4oQsysIwgcoAA&biw=1920&bih=937",
+      query: "PC+intel+i5+8GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk02ZBwi_tLoz6-vp9W57ZDC1TsZ3FA:1615675389597&q=PC&tbas=0&tbs=vw:g,mr:1,pdtr0:1010563%7C1010566,pdtr1:728839%7C728841,cat:325,pdtr2:728797%7C764654&sa=X&ved=0ahUKEwj27aztq67vAhXTLLkGHZ4kB4oQsysIwgcoAA&biw=1920&bih=937",
     },
   },
   laptop: {
     "video-editor": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk03gpAbfSpaQLakXJSL02CrJDvZ0YA:1615675425548&q=Notebook&tbs=vw:g,mr:1,pdtr0:1020720%7C64.0%2464.0,cat:328,pdtr1:1020484%7C4000.0%244000.0&sa=X&ved=0ahUKEwjSlL_-q67vAhXjH7kGHVH_BsEQvSsI9wgoBg&biw=1920&bih=937",
+      query: "notebook+intel+i9+32GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk03gpAbfSpaQLakXJSL02CrJDvZ0YA:1615675425548&q=Notebook&tbs=vw:g,mr:1,pdtr0:1020720%7C64.0%2464.0,cat:328,pdtr1:1020484%7C4000.0%244000.0&sa=X&ved=0ahUKEwjSlL_-q67vAhXjH7kGHVH_BsEQvSsI9wgoBg&biw=1920&bih=937",
     },
     "software-development": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01iIYzi0fbTYoHg20ztnucm29HZRA:1615675484079&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:976847%7C976848,price:1,ppr_min:6000,ppr_max:10000,pdtr1:703981%7C764396,pdtr2:1020484%7C1000.0%24,cat:328,pdtr3:1337889%7C4192014&sa=X&ved=0ahUKEwiVwbOarK7vAhVZHLkGHU61DyAQsysI5wgoAA&biw=1920&bih=937",
+      query: "notebook+intel+i7+32GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01iIYzi0fbTYoHg20ztnucm29HZRA:1615675484079&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:976847%7C976848,price:1,ppr_min:6000,ppr_max:10000,pdtr1:703981%7C764396,pdtr2:1020484%7C1000.0%24,cat:328,pdtr3:1337889%7C4192014&sa=X&ved=0ahUKEwiVwbOarK7vAhVZHLkGHU61DyAQsysI5wgoAA&biw=1920&bih=937",
     },
     "office-tools": {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01AnaoOG0louRMuHvjiF6CuTQD_pg:1615675519042&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:703981%7C764396,cat:328,pdtr1:1020720%7C6.0%246.0&sa=X&ved=0ahUKEwjex4mrrK7vAhXwIrkGHUxfDzMQvSsIhgkoAw&biw=1920&bih=937",
+      query: "notebook+intel+i3+8GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01AnaoOG0louRMuHvjiF6CuTQD_pg:1615675519042&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:703981%7C764396,cat:328,pdtr1:1020720%7C6.0%246.0&sa=X&ved=0ahUKEwjex4mrrK7vAhXwIrkGHUxfDzMQvSsIhgkoAw&biw=1920&bih=937",
     },
     internet: {
-      url:
-        "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01AnaoOG0louRMuHvjiF6CuTQD_pg:1615675519042&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:703981%7C764396,cat:328,pdtr1:1020720%7C6.0%246.0&sa=X&ved=0ahUKEwjex4mrrK7vAhXwIrkGHUxfDzMQvSsIhgkoAw&biw=1920&bih=937",
+      query: "notebook+intel+i5+8GB",
+      url: "https://www.google.com/search?hl=en&tbm=shop&sxsrf=ALeKk01AnaoOG0louRMuHvjiF6CuTQD_pg:1615675519042&q=Notebook&tbas=0&tbs=vw:g,mr:1,pdtr0:703981%7C764396,cat:328,pdtr1:1020720%7C6.0%246.0&sa=X&ved=0ahUKEwjex4mrrK7vAhXwIrkGHUxfDzMQvSsIhgkoAw&biw=1920&bih=937",
     },
   },
 };
@@ -267,15 +268,29 @@ function UsageScreen({ navigation }: UsageScreenProps) {
 type LoadingScreenProps = StackScreenProps<StackParamList, "ResultScreen">;
 
 function LoadingScreen({ navigation }: LoadingScreenProps) {
-  React.useEffect(() => {
-    const timeout = setTimeout(() => {
-      navigation.replace("ResultScreen");
-    }, 800);
+  const { usage, device } = surveyStore(
+    (state) => ({
+      usage: state.usage,
+      device: state.device,
+      setUsage: state.setSurvey("usage"),
+    }),
+    shallow
+  );
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
+  const query = RESPONSES[device][usage].query;
+
+  const { data, isSuccess } = useQuery(["results", query], () => {
+    return fetch(
+      `https://afternoon-bayou-13013.herokuapp.com/test?${new URLSearchParams({
+        q: query,
+      })}`
+    ).then((res) => res.json());
+  });
+
+  useEffect(() => {
+    navigation.replace("ResultScreen", { result: data });
+  }, [isSuccess]);
+
   return (
     <View style={styles.container}>
       <ActivityIndicator size={128} />
@@ -288,74 +303,42 @@ function LoadingScreen({ navigation }: LoadingScreenProps) {
 
 type ResultScreenProps = StackScreenProps<StackParamList, "ResultScreen">;
 
-function ResultScreen({ navigation }: ResultScreenProps) {
-  const { device, usage } = surveyStore(
-    (state) => ({
-      device: state.device,
-      usage: state.usage,
-    }),
-    shallow
-  );
+function ResultScreen({ navigation, route }: ResultScreenProps) {
+  const result = route.params.result;
 
-  return (
-    <ScrollView
-      style={{ paddingLeft: 32, paddingRight: 32, backgroundColor: "white" }}
-    >
-      <Title>Resultado</Title>
-      <Paragraph>
-        Você esta em busca de um {DEVICE_ANSWERS[device]} a ser usado{" "}
-        {USAGE_ANSWERS[usage]}.
-      </Paragraph>
-      <Paragraph>
-        Abaixo temos o resultado da configuração recomendada e links para
-        ajuda-los a encontrar seu {DEVICE_ANSWERS[device]} pela internet.
-      </Paragraph>
-      <Paragraph>
-        Obs: Os valores abaixo estão atualmente mockados. Os links são
-        dinâmicos, mas não refletem uma busca real.
-      </Paragraph>
-      <Spacer height={24} />
-      <Card elevation={4}>
-        <Card.Title title="Especificações" />
+  const renderItem = ({ item }) => {
+    console.log(item);
+    return (
+      <Card>
         <Card.Content>
-          <DataTable>
-            <DataTable.Row>
-              <DataTable.Cell>Memoria</DataTable.Cell>
-              <DataTable.Cell numeric>159</DataTable.Cell>
-            </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell>HD</DataTable.Cell>
-              <DataTable.Cell numeric>237</DataTable.Cell>
-            </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell>Processador</DataTable.Cell>
-              <DataTable.Cell numeric>Intel</DataTable.Cell>
-            </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell>Vel. do Processador</DataTable.Cell>
-              <DataTable.Cell numeric>3.3 GHz</DataTable.Cell>
-            </DataTable.Row>
-          </DataTable>
+          <Title>{item.title}</Title>
+          <Paragraph>{item.link}</Paragraph>
         </Card.Content>
+        <Card.Cover source={{ uri: item.image }} />
         <Card.Actions>
-          <View style={{ flexDirection: "column" }}>
-            <Button
-              onPress={async () => {
-                let result = await WebBrowser.openBrowserAsync(
-                  RESPONSES[device][usage].url
-                );
-                console.log(result);
-              }}
-            >
-              Buscar no Google
-            </Button>
-            <Button disabled>Buscar na Amazon</Button>
-          </View>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
         </Card.Actions>
       </Card>
-      <Spacer height={32} />
+    );
+  };
+
+  return (
+    <View
+      style={{
+        paddingLeft: 32,
+        paddingRight: 32,
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
+      <FlatList
+        data={result}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.title}
+      />
       <Button onPress={() => navigation.popToTop()}>Recomeçar</Button>
-    </ScrollView>
+    </View>
   );
 }
 
@@ -363,17 +346,19 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Presentation" component={Presentation} />
-          <Stack.Screen name="DeviceScreen" component={DeviceScreen} />
-          <Stack.Screen name="UsageScreen" component={UsageScreen} />
-          <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-          <Stack.Screen name="ResultScreen" component={ResultScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Presentation" component={Presentation} />
+            <Stack.Screen name="DeviceScreen" component={DeviceScreen} />
+            <Stack.Screen name="UsageScreen" component={UsageScreen} />
+            <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+            <Stack.Screen name="ResultScreen" component={ResultScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
 
